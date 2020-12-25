e72ccb0ac796d94322ee99e7741bc6d83e8bf38a
khomsan <khomsan_su@cmu.ac.th>
2020-12-25 17:06:29 +0700

    lab_6 done

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 char before(char x){
+	if(x=='A'){
+		return 'Z';
+	}
+	else if(x>'A'&& x<='Z'){
+		return x-1;
+	}
+	else{
+		return '0';
+	}
 	//Write your function definition here
 }
 
